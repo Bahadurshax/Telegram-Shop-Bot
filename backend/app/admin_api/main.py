3d6f66f5d29b037f39eb8dab7c12a2b8f45a23f3
Bahadurshax <arzimbetov.bahadir@inbox.ru>
2026-01-01 15:38:16 +0500

    quick fixes

--- backend/app/admin_api/main.py
+++ backend/app/admin_api/main.py
@@ -34,7 +34,8 @@ def create_admin_app() -> FastAPI:
             "http://localhost:3000", 
             "http://127.0.0.1:3000",
             "http://localhost:5173",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+            "https://tg-mini-app-925f0.web.app"
         ],
         allow_credentials=True,
         allow_methods=["*"],
